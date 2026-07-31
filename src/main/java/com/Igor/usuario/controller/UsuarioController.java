c63a422dbea4db82f6b9e8949ff4d676fb05b1bc
Igor Godoi <ig.godoi00@gmail.com>
2026-07-31 21:33:31 +0100

    Add metodo que salva dados do usuario

---
+++ src/main/java/com/Igor/usuario/controller/UsuarioController.java
@@ -0,0 +1,27 @@
+package com.Igor.usuario.controller;
+
+import com.Igor.usuario.business.UsuarioService;
+import com.Igor.usuario.business.dto.UsuarioDTO;
+import com.Igor.usuario.infrastructure.entity.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+
+
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+}
