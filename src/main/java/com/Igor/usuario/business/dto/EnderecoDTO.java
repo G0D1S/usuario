2c3f646a8edef29a5990a3973c7b30dc87a62d94
Igor Godoi <ig.godoi00@gmail.com>
2026-09-19 21:13:49 +0100

    fix do PRIVATE LONG ID

--- src/main/java/com/Igor/usuario/business/dto/EnderecoDTO.java
+++ src/main/java/com/Igor/usuario/business/dto/EnderecoDTO.java
@@ -11,7 +11,7 @@ import lombok.*;
 @Builder
 public class EnderecoDTO {
 
-    private long id;
+    private Long id;
     private String rua;
     private Long numero;
     private String complemento;
